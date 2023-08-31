d2c1c72e40e63cebfe3fc9e9c4293e86511ec0b7
davin0130 <davin0130@naver.com>
2023-08-31 10:27:49 +0900

    Create:App:portfolio foler

---
+++ src/main/java/com/portfolio/back/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.portfolio.back.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String hi() {
+        return "Hello, world!";
+    }
+}
