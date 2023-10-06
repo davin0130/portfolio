540e9389ad925db86571304fe9de4050392912fb
davin0130 <davin0130@naver.com>
2023-10-06 18:36:19 +0900

    Modify:Design:Change Design MainPage

--- src/main/front/src/pages/Home.js
+++ src/main/front/src/pages/Home.js
@@ -1,10 +1,8 @@
-import { Link } from "react-router-dom";
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-import Header, { LinkItem } from './Header';
-import Footer from './Footer';
-import '../styles/Home.css';
+import { LinkItem } from './Header';
+import '../assets/styles/Home.css';
 
 function Home() {
   const [hello, setHello] = useState('')
@@ -14,28 +12,43 @@ function Home() {
       .then(response => setHello(response.data))
       .catch(error => console.log(error))
   }, []);
-  const fontStyle={
-    color: "#DB9700",
-    }
+  const fontTitle={
+    color: "lightgray",
+    fontSize: "150px",
+    float: "right",
+    border: "1px solid red",
+  }
+  const fontPoint={
+    color: "rgb(66, 199, 241)",
+    fontWeight: "bold",
+  }
+  
   return (
     <div>
       <div className="home">
-          <div className="circle rotate_circle"></div>
+          {/* <div className="circle rotate_circle"></div> */}
           <div className="content1">
-            <div className="content1-left">
-                박스
-            </div>
+            <div className="main-image"></div>
             <div className="content1-right">
-              더 나은 소비자의 일상을 생각하GO,<br />
-              파트너와 원활하게 협업하GO,<br />
-              목표하GO, 성취하는<br />
-              <h1 style={fontStyle}>SERVICE PLANNER</h1>
-              웹 서비스 기획자 정다빈입니다<br />
+              <p className="main-title" style={fontTitle}>PRODUCT MANAGER</p>
+              <p className="cr1 doong1">더 나은 소비자의 일상을 생각하<span style={fontPoint}>GO</span></p>
+              <p className="cr2 doong2">파트너와 원활하게 협업하<span style={fontPoint}>GO</span></p>
+              <p className="cr3 doong1">목표하<span style={fontPoint}>GO</span> 성취하는</p>
+              
             </div>
           </div>
 
-          <LinkItem to="/" className="play">Play with me!</LinkItem>
+          <LinkItem to="/" className="play">pL4y</LinkItem>
 
+          {/* <div className="content2">
+            <div className="">
+              기획자는
+              좋은 아이디어를 
+              보기 좋게 
+              잘 만들 수 있도록
+              설계, 계획하고 관리하는 사람
+            </div>
+          </div>
           <p className="content2-title doong1">#Record</p>
           <a href="/intro">더보기</a>
           <div className="content2">
@@ -90,7 +103,7 @@ function Home() {
                 시큐어코딩 교육 컨텐츠 개발
                 이건 어떤 프로젝트였다
               </div>
-            </div>
+            </div> */}
             
             {/* 사이버보안 챌린지 통합 플랫폼 리뉴얼 기획<br/>
             시큐어코딩 교육 컨텐츠 개발<br/>
@@ -98,7 +111,7 @@ function Home() {
             개인정보 가명익명 처리 경진대회 PL<br/> */}
           </div>
 
-          <p className="content2-title doong2">#Project</p>
+          {/* <p className="content2-title doong2">#Project</p>
           <div className="">
             교통카드 충전 애플리케이션 취약점 분석<br/>
             TAGO | 공유 모빌리티 웹 애플리케이션 개발<br/>
@@ -109,10 +122,10 @@ function Home() {
           <p className="content2-title doong2">#Experience</p>
           
           K-Shield 주니어<br/>
-          A.idle    <br/>
+          A.idle    <br/> */}
 
 
-        </div>
+        {/* </div> */}
          {hello}
     </div>
   );
